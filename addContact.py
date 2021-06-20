08cfaa24b1ae2bce26dfc8b292f9071d716e826f
Jhun <fecalderon@student.apc.edu.ph>
2021-06-20 23:55:53 +0800

    Initial

--- addContact.py
+++ addContact.py
@@ -1,9 +1,15 @@
 from tkinter import *
+from myFormat import *
 window =Tk()
 window.geometry("480x480")
 window.title("Add a Contact")
 
-addContactLabel = Label(window,text="Add a Contact",font=("arial",16,"bold")).pack()
+Label(window,text="Add a Contact",font=Fonts.fontTitle).pack()
 
+Label(text="Name",font=Fonts.labelFormat).pack()
+inputName = Text(window,height=1,width=30,bg="white").pack()
+Label(text="Contact",font=Fonts.labelFormat).pack()
+inputContact = Text(window,height=1,width=30,bg="white").pack()
+Label(text="Email Address")
 
 window.mainloop()
---
+++ myFormat.py
@@ -0,0 +1,5 @@
+class Fonts():
+    fontTitle = ("arial",18,"bold")
+    fontButton = ("arial",16)
+    labelFormat = ("arial",14)
+
